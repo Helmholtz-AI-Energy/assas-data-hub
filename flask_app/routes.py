9dfb601d1ab32a02fa19c7fec9259870411eea34
root <root@assas.scc.kit.edu>
2024-03-14 18:56:06 +0100

    added restful api via uuid

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,8 +1,10 @@
 '''Routes for parent Flask app.'''
 import datetime
 
-from flask import redirect, render_template
+from flask import redirect, render_template, send_file
 from flask import current_app as app
+
+from assasdb import AssasDatabaseManager
 
 @app.route('/')
 def from_root_to_home():
@@ -14,13 +16,14 @@ def from_app_to_home():
     
     return redirect('/assas_app/home')
 
-#@app.route('/')
-#def home():
-#    '''Landing page.'''
-#    return render_template('index.html',
-#        utc_dt=datetime.datetime.utcnow(),
-#        title='Plotly Dash Flask Tutorial',
-#        description='Embed Plotly Dash into your Flask applications.',
-#        template='home-template',
-#        body='This is a homepage served with Flask.'
-#    )
+@app.route('/assas_app/files/<uuid>', methods=['GET'])
+def get_data_files(uuid):
+    
+    manager = AssasDatabaseManager(
+        app.config.get('LOCAL_ARCHIVE'), 
+        app.config.get('LSDF_ARCHIVE'))
+    
+    document = manager.get_database_entry_uuid(uuid)
+    filepath = document['system_path'] + '/dataset.h5'
+    
+    return send_file(filepath)
