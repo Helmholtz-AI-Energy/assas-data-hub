5107d76aa75967fda6b9fb8b9fc7b47f47a6cfed
root <root@assas.scc.kit.edu>
2024-03-18 16:44:40 +0100

    adapted settings to target

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config(object):
     DEBUG = True
     DEVELOPMENT = True
     LSDF_ARCHIVE = r'/mnt/ASSAS/upload/'
-    LOCAL_ARCHIVE = r'/home/jonas/upload/'
-    PYTHON_VERSION = r'python3.10'
-    ASTEC_ROOT = r'/home/jonas/astecV3.1/code/proc/astec.py' 
-    ASTEC_PARSER = r'/home/jonas/assas-data-hub/assas_database/assasdb/assas_astec_parser.py'
+    LOCAL_ARCHIVE = r'/root/upload/'
+    PYTHON_VERSION = r'/opt/python/3.11.8/bin/python3.11'
+    ASTEC_ROOT = r'/root/astecV3.1.1_linux64/astecV3.1.1/code/proc/astec.py' 
+    ASTEC_PARSER = r'/root/assas-data-hub/assas_database/assasdb/assas_astec_parser.py'
     
     #SECRET_KEY = 'do-i-really-need-this'
     #FLASK_HTPASSWD_PATH = '/secret/.htpasswd'
